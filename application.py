14d5aaf3a538dc42fe77965f269f25ab00fb10af
Rosie rose rose <rvl24@cornell.edu>
2019-05-04 16:23:39 -0400

    Update application

--- application.py
+++ application.py
@@ -2,10 +2,6 @@ import os
 
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
-from werkzeug.security import check_password_hash, generate_password_hash
-
 
 app = Flask(__name__)
 
