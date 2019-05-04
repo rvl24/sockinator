5908d7bbb0447f9408b750d8461d3dba6794aa80
Rosie rose rose <rvl24@cornell.edu>
2019-05-04 16:16:26 -0400

    try again

--- application.py
+++ application.py
@@ -1,14 +1,13 @@
 import os
 
-from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-app = Flask(__name__)
 
-db = SQL("https://data.heroku.com/datastores/5a1cc4d0-c58e-4bd7-b61f-0baa18e96e51")
+
+app = Flask(__name__)
 
 
 @app.route('/')
