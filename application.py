dc0eb98c12a830fcb2938f21d85d449b69b62f3f
rvl24 <rvl24@cornell.edu>
2019-05-08 21:55:17 +0000

    testing db connection

--- application.py
+++ application.py
@@ -1,18 +1,147 @@
 import os
-
+from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
 
+from helpers import apology, login_required, lookup
+
+# Configure application
 app = Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-@app.route('/')
-def homepage():
-    return """
-    <h1>Hello, World! This is just a test</h1>
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use Heroku PostgreSQL database
+db = SQL("postgres://djguwhovfgmtjf:3b8fb50c286e296f59a503a6893348914c572b77073848d9835a94120818dd13@ec2-50-19-127-115.compute-1.amazonaws.com:5432/d4h7gvb1ahdvmu")
+
+
+@app.route("/")
+@login_required
+def index():
+    """Show sock drawer"""
     
-    """
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return apology("Username required")
+            
+        # Look up username in database
+        status = db.execute("SELECT * FROM users WHERE username = :name", name=request.args.get("username"))
+        
+        # If username returned, return false, since username is not available
+        if len(status) > 0:
+            return apology("Username taken")
+
+        # Get password from form
+        password = request.form.get("password")
+        
+        # Check that password has been entered
+        if len(password) > 0:
+            
+            # check whether confirmation is the same as the original
+            confirmation = request.form.get("confirmation")
+            if password != confirmation:
+                return apology("Passwords do not match")
+            
+            # hash the password (method, salt length poached from werkzeug example)
+            password_hash = generate_password_hash(request.form.get("password"), 
+                                                   method='pbkdf2:sha256', salt_length=8)
+            
+            # store username, password hash in database                                       
+            db.execute("INSERT INTO users (username, hash) VALUES (:u, :p)", 
+                       u=request.form.get("username"), p=password_hash)
+            
+            # notify the user that they've been properly registered
+            flash("Registered!")
+            return redirect("/")
+        else: 
+            return apology("Please enter a password")
+    else:
+        return render_template("register.html")
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
